d4dff81d7c6e123c128bb3df1284e735987e844d
almazankaze <jalmaz3@uic.edu>
2020-07-19 15:48:28 -0500

    added view to see roles of team

---
+++ BugTracker/Controllers/AdministrationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult RolesList()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+    }
+}
